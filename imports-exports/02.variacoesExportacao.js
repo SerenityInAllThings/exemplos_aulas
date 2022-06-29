968f8ec2369de02cc7f74621af392f4c7e099108
SerenityInAllThings <petersonvgama@gmail.com>
2022-06-29 02:29:06 -0300

    Adding final slides code

--- imports-exports/02.variacoesExportacao.js
+++ imports-exports/02.variacoesExportacao.js
@@ -1,3 +1,37 @@
-const meuValor = 10
+// module.exports.meuValor = 10
+// module.exports.minhaFuncao = (a, b) => {
+//   return a + b
+// }
 
-module.exports.meuValor = meuValor
+// // Alternativa 1
+const meuValor = 10
+const maisUmValor = 11
+const valorASerRenomeado = 12
+const minhaFuncao = (a, b) => {
+  return a + b
+}
+
+module.exports = { 
+  meuValor,
+  maisUmValor,
+  renomeado: valorASerRenomeado,
+  minhaFuncao,
+}  
+
+// // Alternativa 2
+// module.exports.meuValor = 10
+// module.exports.maisUmValor = 11
+// module.exports.renomeado = 12
+// module.exports.minhaFuncao = (a, b) => {
+//   return a + b
+// }
+
+// // Alternativa 3
+// module.exports = { 
+//   meuValor: 10,
+//   maisUmValor: 11,
+//   renomeado: 12,
+//   minhaFuncao: (a, b) => {
+//     return a + b
+//   },
+// } 
